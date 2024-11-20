a2ff07a02b11e8099e29ebd0a0d64e06cf0390e4
Volodya <gtapsplus1@yandex.kz>
2024-11-20 14:56:17 +0300

    lab5guest

--- src/components/RoadCard.tsx
+++ src/components/RoadCard.tsx
@@ -19,12 +19,12 @@ const RoadCard: React.FC<RoadCardProps> = ({ road }) => {
             <div className="card-body">
                 <h5 className="card-title">{road.name}</h5>
                 <p className="card-text">Разрешенная скорость: {road.speed} км/ч</p>
-                {road.value !== undefined && (
+                {/* {road.value !== undefined && (
                     <div className="form-group">
                         <label>Ночь</label>
                         <input className="form-control" value={road.value ? "Да" : "Нет"} disabled />
                     </div>
-                )}
+                )} */}
                 <Link to={`/roads/${road.id}`} className="btn btn-primary">Открыть</Link>
             </div>
         </div>
